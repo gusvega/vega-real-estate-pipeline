5b7bdb17a7ddb51ea33a8980ed75c20acfe975a2
gusvega <gux.vega@gmail.com>
2023-11-06 16:09:47 -0700

    working on automation

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ variable "azure_client_secret" {}
 resource "azurerm_kubernetes_cluster" "production" {
   name                = "real-estate-cluster"
   location            = "westus"
-  resource_group_name = "pipeline"
+  resource_group_name = "vega_real_estate"
   dns_prefix          = "real-estate-cluster"
 
   default_node_pool {
